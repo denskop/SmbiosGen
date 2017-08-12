0858172c82f7908fc197490edd54a0b176307cdc
denskop <denskop.dev@gmail.com>
2017-08-12 19:39:14 +0300

    Init project

---
+++ smbios-gen/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  smbios-gen
+//
+//  Created by Mr.Dee on 12.08.17.
+//  Copyright © 2017 denskop. All rights reserved.
+//
+
+#include <stdio.h>
+#include "specification/types.h"
+
+int main(int argc, const char * argv[]) {
+}
---
+++ smbios-gen/specification/types.h
@@ -0,0 +1,18 @@
+//
+//  types.h
+//  smbios-gen
+//
+//  Created by Mr.Dee on 12.08.17.
+//  Copyright © 2017 denskop. All rights reserved.
+//
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned long int QWORD;
+
+#include "type0.h"
+#endif /* __TYPES_H__ */
