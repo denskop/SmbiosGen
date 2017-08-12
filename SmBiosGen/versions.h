7268ca51dcb5a9f98526581aa74f16e22d0c7907
denskop <denskop.dev@gmail.com>
2017-08-13 00:24:35 +0300

    Add SMBIOS versions

---
+++ SmBiosGen/versions.h
@@ -0,0 +1,24 @@
+//
+//  versions.h
+//  SmBiosGen
+//
+//  Created by Mr.Dee on 13.08.17.
+//  Copyright © 2017 denskop. All rights reserved.
+//
+
+#ifndef __VERSIONS_H__
+#define __VERSIONS_H__
+
+#define SMBIOS_VER_2_0  20
+#define SMBIOS_VER_2_1  21
+#define SMBIOS_VER_2_2  22
+#define SMBIOS_VER_2_3  23
+#define SMBIOS_VER_2_4  24
+#define SMBIOS_VER_2_5  25
+#define SMBIOS_VER_2_6  26
+#define SMBIOS_VER_2_7  27
+#define SMBIOS_VER_2_8  28
+#define SMBIOS_VER_3_0  30
+#define SMBIOS_VER_3_1  31
+
+#endif /* __VERSIONS_H__ */
